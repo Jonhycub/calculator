eb05b45e7b5312126d745dc2e4c14825f2952555
Jonhycub <jonhycub@gmail.com>
2023-12-30 20:41:30 -0500

    Added the basic operation functions

---
+++ scripts.js
@@ -0,0 +1,39 @@
+//Create a function to add
+function add(num1, num2){
+  return num1 + num2;
+}
+
+//Create a function to subtract
+function substract(num1, num2){
+  return num1 - num2;
+}
+
+//Create a function to multiply
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+//Create a function to divide
+function divide(num1, num2){
+  return num1 / num2;
+}
+
+//Create variables for the numbers and the operator
+let num1;
+let num2;
+let operator = "";
+
+//Create a function operate() that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+function operate(operator, num1, num2){
+  if(operator == '+'){
+    return add(num1, num2);
+  }else if(operator == '-'){
+    return substract(num1, num2);
+  }else if(operator == '*'){
+    return multiply(num1, num2);
+  }else if(operator == '÷'){
+    return divide(num1, num2);
+  }
+}
+
+console.log(operate('-', 100, 10));
